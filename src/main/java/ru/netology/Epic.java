f123618548facf1e22fae0bbb279334b42bf5956
GorLena <zhura1977@list.ru>
2024-08-05 10:24:22 +0300

    hw

---
+++ src/main/java/ru/netology/Epic.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+public class Epic extends Task {
+    private final String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (String subtask : subtasks) {
+            if (subtask.contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
